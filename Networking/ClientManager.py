4a9ea7dd935609c6baca6ddce007aff593cdb729
maxrc <maxrc@uci.edu>
2015-03-22 16:12:49 -0700

    Initial testing with client/server

---
+++ Networking/ClientManager.py
@@ -0,0 +1,54 @@
+import threading
+import pickle
+import _thread
+from collections import deque
+from socket import *
+
+TCP_IP = '192.168.42.106'
+
+class ClientManager:
+
+    def __init__(self):
+        # Tells whether the server should running
+        # Should always be true, but it's good to include this here for clarity
+        self.running = True
+
+        self.socket = socket(AF_INET, SOCK_STREAM) #TCP
+
+        # Enables ReuseAddr, seems to be not necessary but I have here just in case
+        self.socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+
+        self.socket.connect((TCP_IP, 6969))
+
+        # Gets IP address
+        self.ip = gethostbyname(gethostname())
+        print(self.ip)
+
+        self.messageQueue = deque()
+        self.messageLock = threading.Lock()
+        self.messageThread = threading.Thread(target=self.checkForMessages)
+        self.messageThread.daemon = True
+        self.messageThread.start()
+
+    def getSocket(self): return self.socket
+    def getMessageQueue(self): return self.messageQueue
+
+    # Receives packets(messages) and puts them into queue
+    def checkForMessages(self):
+        while self.running:
+            pickledData = self.socket.recv(4096)
+            data = pickle.loads(pickledData)
+
+            self.messageQueue.append((data, addr))
+            # Remember to lock so that we don't run into conflict accessing it
+            self.messageLock.acquire()
+
+            # Receives a message and puts it into the message queue
+            self.messageQueue.append(data)
+
+            self.messageLock.release()
+
+clientManager = ClientManager()
+
+while True:
+    pass
