dcd08d80d07ec505f3d19bfb8eec368d8b7050f1
VicSto <vstolle@uci.edu>
2015-03-22 23:48:57 -0700

    Updates to Basic_Buttons/Creating Battle_Buttons
    Added a list of keys and IDs, and began the Battle_Buttons class.
    Battle_Buttons will contain the buttons for the battle screen

--- Basic_Buttons.py
+++ Basic_Buttons.py
@@ -11,73 +11,59 @@ LIGHTGRAY = (212, 208, 200)
 
 class Basic_Buttons:
 
-    def __init__(self, rect = None, caption = '', bgcolor = LIGHTGRAY, fgcolor = BLACK, normal = None, down= None, highlight = None):
+    def __init__(self, rect = None, bgcolor = LIGHTGRAY, fgcolor = BLACK, normal = None, ID = ''):
+
+        #List of Keys
+        self.keys = [False, False, False, False, False, False] #key[0] = UP, key[1] = DOWN, key[2] = LEFT, key[3] = RIGHT, key[4] = g, key[5] = h
+        
+        #Button's ID (I.E 'Attack', 'Switch', 'Forfeit')
+        self.ID = ID
+
+        #Button Rectangle
         if rect is None:
             self.rect = pygame.Rect(0,0,30,60)
         else:
             self.rect = pygame.Rect(rect)
 
-        self.caption = caption
+        #Button text/font
+        self.caption = ID
+        self.font = pygame.font.Font('PKMN RBYGSC.tff', 14)
+        
+        #Button foreground/background color
         self.bgcolor = bgcolor
         self.fgcolor = fgcolor
-        self.font = pygame.font.Font('PKMN RBYGSC.tff', 14)
 
-        #tracking the state of the button
-        self.buttonDown = False #is the key down
-        self.keyOnButton = False # is the key currently on the button
-        self.lastKeyOnButton = False # was the last keyPressed on the button
-
+        #is the button visible/is the button an image
         self.visible = True
         self.customSurfaces = False
 
         if normal is None:
             #creates the surface with text
             self.surfaceNormal = pygame.Surface(self.rect.size)
-            self.surfaceDown = pygame.Surface(self.rect.size)
-            self.surfaceHighlight = pygame.Surface(self.rect.size)
-            self.update()
+            self._update()
         else:
             #create the surfaces for a custom image button
-            self.setSurfaces(normal, down, highlight)
+            self.setSurfaces(normal)
 
 
     def draw(self, surfaceObj):
         #Blit the current button's appearance to the surface object
         if self.visible:
-            if self.buttonDown:
-                surfaceObj.blit(self.surfaceDown, self.rect)
-            elif self.keyOnButton:
-                surfaceObj.blit(self.Highlight, self.rect)
-            else:
-                surfaceObj.blit(self.surfaceNormal, self.rect)
+            surfaceObj.blit(self.surfaceNormal, self.rect)
 
-    def setSurfaces(self, normalSurface, downSurface=None, highlightSurface=None):
+    def setSurfaces(self, normalSurface):
         # Drawing the button image...
-        
-
-        # Checks to see if button has downclick image and highlight image
-        if downSurface is None: downSurface = normalSurface
-        if highlightSurface is None: highlightSurface = normalSurface
 
         #checks to see if the surface is a string filepath
         if type(normalSurface) == str: self.origSurfaceNormal = pygame.image.load(normalSurface)
-        if type(downSurface) == str: self.origSurfaceDown = pygame.image.load(downSurface)
-        if type(highlightSurface) == str: self.origSurfaceHighlight = pygame.image.load(highlightSurface)
-
-        #check the image sizes
-        if self.origSurfaceNormal.get_size() != self.origSurfaceDown.get_size() != self.origSurfaceHighlight.get_size(): raise Exception('Error: item sizes do not match.')
         
         #set the surface images
         self.surfaceNormal = self.origSurfaceNormal
-        self.surfaceDown = self.origSurfaceDown
-        self.surfaceHighlight = self.origSurfaceHighlight
         self.customSurfaces = True
         self.rect = pygame.rect((self.rect.left, self.rect.top, self.surfaceNormal.get_width(), self.surfaceNormal.get_height()))
 
-
-
-
     def _update(self):
+        
         #Redraw the button's Surface object. Call when the button is changed
         _w = self.rect.width
         _h = self.rect.height
@@ -86,23 +72,18 @@ class Basic_Buttons:
         #if the button is an image, smoothscale transform the button
         if self.customSurfaces:
             self.surfaceNormal = pygame.transform.smoothscale(self.origSurfaceNormal, self.rect.size)
-            self.surfaceDown = pygame.transform.smoothscale(self.origSurfaceDown, self.rect.size)
-            self.surfaceHighlight = pygame.transform.smoothscale(self.origSurfaceHighlight, self.rect.size)
             return
         
-        #fill background color for all buttons
+        #fill background color for button
         self.surfaceNormal.fill(self.bgcolor)
-        self.surfaceDown.fill(self.bgcolor)
-        self.surfaceHighlight.fill(self.bgcolor)
                              
         #draw caption text for all buttons
         captionSurf = self.font.render(self.caption, True, self.fgcolor, self.bgcolor)
         captionRect = captionSurf.get_rect()
         captionRect.center = int(w/2), int(h/2)
         self.surfaceNormal.blit(captionSurf, captionRect)
-        self.surfaceDown.blit(captionSurf, captionRect)
 
-        #draw border for normal button
+        #draw border for button
         pygame.draw.rect(self.surfaceNormal, BLACK, pygame.Rect((0, 0, w, h)), 1) # Black border around everything
         pygame.draw.line(self.surfaceNormal, WHITE, (1, 1), (w-2, 1))
         pygame.draw.line(self.surfaceNormal, WHITE, (1, 1), (1, h-2))
@@ -110,18 +91,6 @@ class Basic_Buttons:
         pygame.draw.line(self.surfaceNormal, DARKGRAY, (w-1, 1), (w-1, h-1))
         pygame.draw.line(self.surfaceNormal, GRAY, (2, h-2), (w-2, h-2))
         pygame.draw.line(self.surfaceNormal, GRAY, (w-2, 2), (w-2, h-2))
-
-        #draw border for down button
-        pygame.draw.rect(self.surfaceDown, BLACK, pygame.Rect((0, 0, w, h)), 1) # Black border around everything
-        pygame.draw.line(self.surfaceDown, WHITE, (1, 1), (w-2, 1))
-        pygame.draw.line(self.surfaceDown, WHITE, (1, 1), (1, h-2))
-        pygame.draw.line(self.surfaceDown, DARKGRAY, (1, h-1), (w-1, h-1))
-        pygame.draw.line(self.surfaceDown, DARKGRAY, (w-1, 1), (w-1, h-1))
-        pygame.draw.line(self.surfaceDown, GRAY, (2, h-2), (w-2, h-2))
-        pygame.draw.line(self.surfaceDown, GRAY, (w-2, 2), (w-2, h-2))
-
-        #draw border for highlight button
-        self.surfaceHighlight = self.surfaceNormal
 
                              
     def getCaption(self): return caption
