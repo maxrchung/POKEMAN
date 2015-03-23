1b46734ac5984caf395b6e7cca9a0e3b4023f8ee
Winston Liang <winstoyl@uci.edu>
2015-03-22 18:23:10 -0700

    reads in moves (maybe)

--- mechanics.py
+++ mechanics.py
@@ -8,4 +8,18 @@ class move(object):
     def __init__(self,line):
         self.parse(line)
     def parse(self,line):
+        self.buff = False
+        movefile = open(line)
+        moveline = movefile.readline().split(',')
+        self.className = moveline[0]
+        self.moveName = moveline[1]
+        if moveline[2] != 'buff':
+            self.power = moveline[2]
+            self.hitchance = moveline[3]
+            self.type = moveline[4]
+        else:
+            self.buff = True
+            self.buffstat = moveline[4]
+            
+        
         
