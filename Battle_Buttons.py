50436ed2c699211aa0dba5d2ecdd4cb82dc6ebe2
VicSto <vstolle@uci.edu>
2015-03-23 12:38:35 -0700

    Added Battle Window Assets, Created Battle_Window, Edited Battle Buttons
    Created images for the Battle Window, created the Battle_Window.py with
    a working overlay, and fixed some bugs with the Battle_Buttons.py

--- Battle_Buttons.py
+++ Battle_Buttons.py
@@ -5,149 +5,141 @@ class Battle_Buttons:
     #Buttons for Battle 
     def __init__(self):
         self.menuButtons = [Basic_Buttons(ID = 'Fight'), Basic_Buttons(ID = 'Switch'), Basic_Buttons(ID = 'forfeit')]
-        self.pokeButtons = []
-        self.moveButtons = []
-        self.currentButton = 0 
+        self.pokeButtons = [Basic_Buttons(ID = 'Poke 1'), Basic_Buttons(ID = 'Poke 2'), Basic_Buttons(ID = 'Poke 3')]
+        self.moveButtons = [Basic_Buttons(ID = 'Yes'), Basic_Buttons(ID = 'No')]
+        self.currentButton = 0 #used to determine what button you are on
         self.menustate = 0 #0: menu, 1: attack, 2: switch, 3: forfeit
         
     
     def eventHandler(self):
-        self._clock = pygame.time.Clock()
-        
-        self.number_count = 0
 
         for event in pygame.event.get():
-            if(self.number_count > 4000):
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_UP:
-                        self.keys[0] = True
-                    elif event.key == pygame.K_DOWN:
-                        self.keys[1] = True
-                    elif event.key == pygame.K_LEFT:  
-                        self.keys[2] = True
-                    elif event.key == pygame.K_RIGHT:
-                        self.keys[3] = True
-                    elif event.key == pygame.K_g:
-                        self.keys[4] = True
-                    elif event.key == pygame.K_h:
-                        self.keys[5] = True
-                elif event.type == pygame.KEYUP:
-                    if event.key == pygame.K_UP:
-                        self.keys[0] = False
-                    elif event.key == pygame.K_DOWN:
-                        self.keys[1] = False
-                    elif event.key == pygame.K_LEFT:
-                        self.keys[2] = False
-                    elif event.key == pygame.K_RIGHT:
-                        self.keys[3] = False
-                    elif event.key == pygame.K_g:
-                        self.keys[4] = False
-                    elif event.key == pygame.K_h:
-                        self.keys[5] = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    self.keys[0] = True
+                elif event.key == pygame.K_DOWN:
+                    self.keys[1] = True
+                elif event.key == pygame.K_LEFT:  
+                    self.keys[2] = True
+                elif event.key == pygame.K_RIGHT:
+                    self.keys[3] = True
+                elif event.key == pygame.K_g:
+                    self.keys[4] = True
+                elif event.key == pygame.K_h:
+                    self.keys[5] = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_UP:
+                    self.keys[0] = False
+                elif event.key == pygame.K_DOWN:
+                    self.keys[1] = False
+                elif event.key == pygame.K_LEFT:
+                    self.keys[2] = False
+                elif event.key == pygame.K_RIGHT:
+                    self.keys[3] = False
+                elif event.key == pygame.K_g:
+                    self.keys[4] = False
+                elif event.key == pygame.K_h:
+                    self.keys[5] = False
 
 
-            if self.menustate == 0 && self.currentButton == 0:
-                # hovering moves
+        if self.menustate == 0 && self.currentButton == 0:
+            # hovering moves
 
-                if self.keys[1]: #down button
-                    self.currentButton = 2
-                elif self.keys[3]: #right button
-                    self.currentButton = 1
-                elif self.keys[4]: #enter button
-                    self.menustate = 1
-                    self.currentState = 0 
+            if self.keys[1]: #down button
+                self.currentButton = 2
+            elif self.keys[3]: #right button
+                self.currentButton = 1
+            elif self.keys[4]: #enter button
+                self.menustate = 1
+                self.currentState = 0 
+            
+
+        elif self.menustate == 0 && self.currentButton == 1:
+            # hovering switch
+            if self.keys[1]: #down button
+                self.currentButton = 2
+            elif self.keys[2]: #left button
+                self.currentButton = 0
+            elif self.keys[4]: #enter button
+                self.menustate = 2
+                self.currentButton = 0
+            
+        elif self.menustate == 0 && self.currentButton == 2:
+            # hovering forfeit
+            if self.keys[0]: #up button
+                self.currentButton = 0
+            elif self.keys[2]: #left button
+                self.currentButton = 1
+            elif self.keys[3]: #right button
+                self.currentButton = 0
+            elif self.keys[4]: #enter button
+                self.menustate = 2
+                self.currentButton = 0
+
+        elif self.menustate == 1 && self.currentButton == 0:
+            # hovering move 1
+            if self.keys[1]: #down button
+                self.currentButton = 2
+            elif self.keys[3]: #right button
+                self.currentButton = 1
+
+        elif self.menustate == 1 && self.currentButton == 1:
+            # hovering move 2
+            if self.keys[1]: #down button
+                self.currentButton = 2
+            elif self.keys[2]: #left button
+                self.currentButton = 0
+            
+        elif self.menustate == 1 && self.currentButton == 2:
+            #hovering move 3
+            if self.keys[0]: #up button
+                self.currentButton = 0
+            elif self.keys[2]: #left button
+                self.currentButton = 1
+            elif self.keys[3]: #right button
+                self.currentButton = 3
+
+        elif self.menustate == 1 && self.currentButton == 3:
+            #hovering move 4
+            if self.keys[0]: #up button
+                self.currentButton = 1
+            elif self.keys[2]: #left button
+                self.currentButton = 2
+
+        elif self.menustate == 2 && self.currentButton == 0:
+            #hovering poke 1
+            if self.keys[3]: #right button
+                self.currentButton = 1
+
+        elif self.menustate == 2 && self.currentButton == 1:
+            #hovering poke 2
+            if self.keys[2]: #left button
+                self.currentButton = 0
+            elif self.keys[3]: #right button
+                self.currentButton = 2
+
+        elif self.menustate == 2 && self.currentButton == 2:
+            #hovering poke 3
+            if self.keys[2]: #left button
+                self.currentButton = 1
                 
+        elif self.menustate == 3 && self.currentButton == 0:
+            #hovering yes
+            if self.keys[3]: #right button
+                self.currentButton = 1
 
-            elif self.menustate == 0 && self.currentButton == 1:
-                # hovering switch
-                if self.keys[1]: #down button
-                    self.currentButton = 2
-                elif self.keys[2]: #left button
-                    self.currentButton = 0
-                elif self.keys[4]: #enter button
-                    self.menustate = 2
-                    self.currentButton = 0
-                
-            elif self.menustate == 0 && self.currentButton == 2:
-                # hovering forfeit
-                if self.keys[0]: #up button
-                    self.currentButton = 0
-                elif self.keys[2]: #left button
-                    self.currentButton = 1
-                elif self.keys[3]: #right button
-                    self.currentButton = 0
-                elif self.keys[4]: #enter button
-                    self.menustate = 2
-                    self.currentButton = 0
+        elif self.menustate == 3 && self.currentButton == 1:
+            #hovering no
+            if self.keys[2]: #left button
+                self.currentButton = 0
 
-            elif self.menustate == 1 && self.currentButton == 0:
-                # hovering move 1
-                if self.keys[1]: #down button
-                    self.currentButton = 2
-                elif self.keys[3]: #right button
-                    self.currentButton = 1
-                elif self.keys[4]: #enter button
 
-            elif self.menustate == 1 && self.currentButton == 1:
-                # hovering move 2
-                if self.keys[1]: #down button
-                    self.currentButton = 2
-                elif self.keys[2]: #left button
-                    self.currentButton = 0
-                
-            elif self.menustate == 1 && self.currentButton == 2:
-                #hovering move 3
-                if self.keys[0]: #up button
-                    self.currentButton = 0
-                elif self.keys[2]: #left button
-                    self.currentButton = 1
-                elif self.keys[3]: #right button
-                    self.currentButton = 3
-
-            elif self.menustate == 1 && self.currentButton == 3:
-                #hovering move 4
-                if self.keys[0]: #up button
-                    self.currentButton = 1
-                elif self.keys[2]: #left button
-                    self.currentButton = 2
-
-            elif self.menustate == 2 && self.currentButton == 0:
-                #hovering poke 1
-                if self.keys[3]: #right button
-                    self.currentButton = 1
-
-            elif self.menustate == 2 && self.currentButton == 1:
-                #hovering poke 2
-                if self.keys[2]: #left button
-                    self.currentButton = 0
-                elif self.keys[3]: #right button
-                    self.currentButton = 2
-
-            elif self.menustate == 2 && self.currentButton == 2:
-                #hovering poke 3
-                if self.keys[2]: #left button
-                    self.currentButton = 1
+        if self.menustate == 1 || self.menustate == 2 || self.menustate == 3:
+            if self.keys[5]: #escape button
+                self.menustate = 0
+                self.currentButton = 0
+               
                     
-            elif self.menustate == 3 && self.currentButton == 0:
-                #hovering yes
-                if self.keys[3]: #right button
-                    self.currentButton = 1
-
-            elif self.menustate == 3 && self.currentButton == 1:
-                #hovering no
-                if self.keys[2]: #left button
-                    self.currentButton = 0
-
-
-            if self.menustate == 1 || self.menustate == 2 || self.menustate == 3:
-                if self.keys[5]: #escape button
-                    self.menustate = 0
-                    self.currentButton = 0
-                   
-                    
-        self.number_count += self._clock.tick()
     
-
-    def executeButton(self, theButton):
         
         
---
+++ Battle_Window.py
@@ -0,0 +1,45 @@
+import pygame, sys
+    
+from pygame.locals import *
+pygame.init()
+
+
+GREEN = ( 0, 255, 0)
+YELLOW = (255, 255, 0)
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0,0,0)
+
+
+
+myfont = pygame.font.Font("PKMN RBYGSC.ttf", 25)
+overlay = pygame.image.load("Main_Overlay.png")
+overlayRect = overlay.get_rect()
+
+
+padx = 20
+pady = 20
+screenwidth = 800
+screenheight = 600
+
+currentHealth = 100
+totalHealth = 100
+        
+display = pygame.display.set_mode((screenwidth, screenheight), 0, 32)
+display.fill(WHITE)
+
+display.blit(overlay, overlayRect)
+poke1 = myfont.render("BULBASAUR", 1, BLACK)
+display.blit(poke1, (530, 260))
+pygame.draw.rect(display, GREEN, (111, 60, 239, 8))
+poke2 = myfont.render("CHARMANDER", 1, BLACK)
+display.blit(poke2, (padx + 80, pady))
+pygame.draw.rect(display, GREEN, (531, 297, 239, 8))
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
