0bc019d53d11888ee9cd297cfad2376adffcec11
Winston Liang <winstoyl@uci.edu>
2015-03-23 15:29:03 -0700

    tits

--- Networking/Server/pokeman.py
+++ Networking/Server/pokeman.py
@@ -23,9 +23,11 @@ class pokeman(object):
         '''
         self.type = type
         self.gender = gender
-        self.setmoves()
         self.moveset=[]
         self.stats=[]
+        self.setmoves()
+        self.setStats()
+        
     def setmoves(self):
         a=random.randint(0,3)
         b=random.randint(0,3)
@@ -34,54 +36,79 @@ class pokeman(object):
         self.moveset.append(moves[self.type*4+a])
         self.moveset.append(moves[self.type*4+b])
         a=random.randint(0,19)
-        while(a/4==self.type):
+        while(int(a/4)==self.type):
             a=random.randint(0,19) 
         b=random.randint(0,19)
-        while(a==b and b/4==self.type):
+        while(a==b or int(b/4)==self.type):
             b=random.randint(0,19)
         self.moveset.append(moves[a])
         self.moveset.append(moves[b])
         
     def setStats(self):
-        global statBase
+        statBase = pokeman.statBase
         if self.type == 0:
-            a=random.randint(15)
+            a=random.randint(0,15)
             self.stats.append(statBase[3]+a)
+            a=random.randint(0,15)
             self.stats.append(statBase[2]+a)
+            a=random.randint(0,15)
             self.stats.append(statBase[4]+a)
+            a=random.randint(0,15)
             self.stats.append(statBase[1]+a)
+            a=random.randint(0,15)
             self.stats.append(statBase[0]+a)
+            a=random.randint(0,15)
             self.stats.append(statBase[2]+a)
         if self.type == 1:
-            a=random.randint(15)
+            a=random.randint(0,15)
             self.stats.append(statBase[0]+a)
+            a=random.randint(0,15)
             self.stats.append(statBase[1]+a)
+            a=random.randint(0,15)
             self.stats.append(statBase[3]+a)
+            a=random.randint(0,15)
             self.stats.append(statBase[3]+a)
+            a=random.randint(0,15)
             self.stats.append(statBase[4]+a)
+            a=random.randint(0,15)
             self.stats.append(statBase[1]+a)
         if self.type == 2:
-            a=random.randint(15)
+            a=random.randint(0,15)
             self.stats.append(statBase[2]+a)
+            a=random.randint(0,15)
             self.stats.append(statBase[0]+a)
+            a=random.randint(0,15)
             self.stats.append(statBase[5]+a)
+            a=random.randint(0,15)
             self.stats.append(statBase[2]+a)
+            a=random.randint(0,15)
             self.stats.append(statBase[1]+a)
+            a=random.randint(0,15)
             self.stats.append(statBase[2]+a)
         if self.type == 3:
-            a=random.randint(15)
+            a=random.randint(0,15)
             self.stats.append(statBase[5]+a)
+            a=random.randint(0,15)
             self.stats.append(statBase[1]+a)
+            a=random.randint(0,15)
             self.stats.append(statBase[1]+a)
+            a=random.randint(0,15)
             self.stats.append(statBase[1]+a)
+            a=random.randint(0,15)
             self.stats.append(statBase[3]+a)
+            a=random.randint(0,15)
             self.stats.append(statBase[3]+a)
         if self.type == 4:
-            a=random.randint(15)
+            a=random.randint(0,15)
             self.stats.append(statBase[3]+a)
+            a=random.randint(0,15)
             self.stats.append(statBase[1]+a)
+            a=random.randint(0,15)
             self.stats.append(statBase[4]+a)
+            a=random.randint(0,15)
             self.stats.append(statBase[1]+a)
+            a=random.randint(0,15)
             self.stats.append(statBase[3]+a)
+            a=random.randint(0,15)
             self.stats.append(statBase[1]+a)
         self.current=0+self.stats[5]
