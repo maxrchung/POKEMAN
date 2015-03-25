d5ffba45deb713917bd9d6702642072d36159d1c
Winston Liang <winstoyl@uci.edu>
2015-03-25 00:50:49 -0700

    battletext moar

--- Networking/Client/Battletext.py
+++ Networking/Client/Battletext.py
@@ -17,10 +17,21 @@ def battletext (game,packet):
             battlestr += game.pokemans[game.activePoke].name + ' used '
         elif packet[1] == 1:
             battlestr += game.oppPoke.name + ' used '
-        if packet[3].buff == True:
-            battlestr += packet[3].moveName + ', ' + packet[3].buffstat + ' has risen!'
-        else:
             battlestr += packet[3].moveName
+        if packet[3].buff == True:
+            if packet[3].buffstat == 0:
+                battlestr += ', atk has risen!'
+            elif packet[3].buffstat == 1:
+                battlestr += ', def has risen!'
+            elif packet[3].buffstat == 2:
+                battlestr += ', satk has risen!'
+            elif packet[3].buffstat == 3:
+                battlestr += ', sdef has risen!'
+            elif packet[3].buffstat == 4:
+                battlestr += ', spd has risen!'
+            elif packet[3].buffstat == 5:
+                battlestr += ', he healed!'
+        else:
             if effectiveness(packet[3],game.oppPoke) == 0.5:
                 battlestr += ', it was not very effective...'
             elif effectiveness(packet[3],game.oppPoke) == 2:
@@ -29,3 +40,4 @@ def battletext (game,packet):
                 battlestr += '!'
     else:
         raise TypeError('???')
+    return battlestr.upper()
